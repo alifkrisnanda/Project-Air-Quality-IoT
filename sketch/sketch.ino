0471f151828aaf035d1cadf23e8b560c993a7f22
alifkrisnanda <alifkrisnanda023@gmail.com>
2023-12-10 22:32:49 +0700

    Update sketch.ino

--- sketch/sketch.ino
+++ sketch/sketch.ino
@@ -11,12 +11,13 @@
 const char* ssid = "Wokwi-GUEST";
 const char* password = "";
 // koneksi thingspeak
-const char* myApiKey = "api key";
-const int myChannelNumber = channelnumber;
+const char* myApiKey = "9DN5RFPRUNLMOVLA";
+const int myChannelNumber = 2358270;
 
 
 // Telegram BOT
-#define BOTtoken "BOTtoken"
+#define BOTtoken "6920662651:AAFxT508ZqHIvIcoZkPaD6gd0FrL-YGOGNw"
+// #define CHAT_ID "1392331206" 
 
 
 #define DHT_PIN 12 
@@ -32,11 +33,15 @@ unsigned long lastTimeBotRan;
 
 
 
+
 void handleNewMessages(int numNewMessages) {
   Serial.println("handleNewMessages");
   Serial.println(String(numNewMessages));
-  float temperature = random(200, 300) / 10.0; // Nilai acak antara 20.0 dan 30.0
-  float humidity = random(400, 600) / 10.0;
+
+  float temperature = random(300, 360) / 10.0;
+  float humidity = random(500, 600) / 10.0;
+  float debu = random(1400, 1500) / 10.0;
+  float co = random(1200, 1300) / 10.0;
 
   for (int i=0; i<numNewMessages; i++) {
     String chat_id = String(bot.messages[i].chat_id);
@@ -51,13 +56,15 @@ void handleNewMessages(int numNewMessages) {
       control += "Gunakan Commands Di Bawah Untuk Monitoring\n\n";
       control += "/temperature Untuk Monitoring Suhu \n";
       control += "/humidity Untuk Monitoring Kelembapan \n";
+      control += "/debu Untuk Monitoring Debu \n";
+      control += "/karbonmonoksida Untuk Monitoring Gas Carbon";
       bot.sendMessage(chat_id, control, "");
     }
 
     if (text == "/temperature") {
       int t= temperature;
       if (isnan(t)) {
-          Serial.println(F("Failed to read from DHT sensor!"));
+          Serial.println(F("Gagal membaca data sensor"));
           return;
       }
       String suhu = "Status Suhu ";
@@ -69,13 +76,35 @@ void handleNewMessages(int numNewMessages) {
     if (text == "/humidity") {
       int h= humidity;
       if (isnan(h)) {
-          Serial.println(F("Failed to read from DHT sensor!"));
+          Serial.println(F("Gagal membaca data sensor"));
           return;
       }
       String lembab = "Status Kelembapan ";
       lembab += h;
       lembab +="%\n";
       bot.sendMessage(chat_id, lembab, "");
+    }
+    if (text == "/debu") {
+      int d= debu;
+      if (isnan(d)) {
+          Serial.println(F("Gagal membaca data sensor"));
+          return;
+      }
+      String debu = "Status Debu ";
+      debu += d;
+      debu +="PPB\n";
+      bot.sendMessage(chat_id, debu, "");
+    }
+    if (text == "/karbonmonoksida") {
+      int c= co;
+      if (isnan(c)) {
+          Serial.println(F("Gagal membaca data sensor"));
+          return;
+      }
+      String co = "Status Karbon Monoksida ";
+      co += c;
+      co +="PPM\n";
+      bot.sendMessage(chat_id, co, "");
     }
   }
 }
@@ -101,19 +130,26 @@ void setup() {
 }
 
 void loop() {
-  float temperature = random(200, 300) / 10.0; // Nilai acak antara 20.0 dan 30.0
-  float humidity = random(400, 600) / 10.0; 
-  
+  float temperature = random(300, 360) / 10.0;
+  float humidity = random(500, 600) / 10.0;
+  float debu = random(1400, 1500) / 10.0;
+  float co = random(1200, 1300) / 10.0;
+
   // Set nilai field di ThingSpeak
   ThingSpeak.setField(1, temperature);
   ThingSpeak.setField(2, humidity);
+  ThingSpeak.setField(3, debu);
+  ThingSpeak.setField(4, co);
 
   // Kirim data ke ThingSpeak
   int x = ThingSpeak.writeFields(myChannelNumber, myApiKey);
 
   // Tampilkan data di Serial Monitor
-  Serial.println("Temperature: " + String(temperature, 2) + "°C");
-  Serial.println("Humidity: " + String(humidity, 1) + "%");
+  Serial.println("Temperature: " + String(temperature, 1) + "°C");
+  Serial.println("Humidity: " + String(humidity, 2) + "%");
+  Serial.println("Debu: " + String(debu, 3) + "PPB");
+  Serial.println("Karbon Monoksida: " + String(co, 4) + "PPM");
+
 
   // Cek status pengiriman data ke ThingSpeak
   if(x == 200){
@@ -136,4 +172,3 @@ void loop() {
     lastTimeBotRan = millis();
   }
 }
-
